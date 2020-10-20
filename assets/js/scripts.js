1cc8b01ab47266ee873eb47cea731b46b0cd145a
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2020-10-20 14:54:51 +0200

    updated menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,14 @@ jQuery(document).ready(function($) {
 		jQuery(this).toggleClass('open');
 		jQuery(".nav-container").slideToggle(500);
 
-		jQuery('body','html').toggleClass("overflow-hidden");
+		// jQuery('body','html').toggleClass("overflow-hidden");
+		jQuery('body','html').toggleClass("h-full");
 	});
+
+	jQuery(".nav-container a").click(function(){
+		jQuery('#nav-icon3').toggleClass('open');
+		jQuery(".nav-container").slideToggle(200);
+
+	})
 });
 
